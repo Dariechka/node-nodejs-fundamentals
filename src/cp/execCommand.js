4dbd30f946a960128744c6b8a98c444a26659510
daryakolenchenko <daria.kolenchenko@gmail.com>
2026-03-04 12:26:40 +0100

    feat: add execCommand solution

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,30 @@
-const execCommand = () => {
+import { spawn } from 'node:child_process';
+
+const execCommand = async () => {
   // Write your code here
   // Take command from CLI argument
   // Spawn child process
   // Pipe child stdout/stderr to parent stdout/stderr
   // Pass environment variables
   // Exit with same code as child
+    if (process.argv.length !== 3) {
+        console.error('Expected one quoted command with arguments, e.g. `node src/cp/execCommand.js \"echo \'Hello World\'\"`');
+        process.exit(1);
+    }
+    const command = process.argv[2];
+
+    const child = spawn(command, {
+        stdio: ['pipe', 'pipe', 'pipe'],
+        env:  process.env,
+        shell: true,
+    })
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+
+    child.on('close', (code) => {
+        process.exit(code);
+    });
 };
 
-execCommand();
+await execCommand();
