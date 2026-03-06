66686518bfb92ed82a995e2dbd40e00c3425223c
daryakolenchenko <daria.kolenchenko@gmail.com>
2026-03-06 15:08:07 +0100

    feat: implement restore script

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,44 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const restore = async () => {
   // Write your code here
   // Read snapshot.json
   // Treat snapshot.rootPath as metadata only
   // Recreate directory/file structure in workspace_restored
+    const target = path.resolve(import.meta.dirname, 'workspace_restored');
+    const exists = async (p) => {
+        try {
+            await fs.access(p);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+    if (await exists(target)) {
+        throw new Error("FS operation failed");
+    }
+    const read = async (json) => {
+        try {
+            return JSON.parse(await fs.readFile(json, 'utf8'));
+        } catch (cause) {
+            throw Error('FS operation failed', {cause});
+        }
+    }
+    const json = await read(path.resolve(import.meta.dirname, 'snapshot.json'));
+    for (const entry of json.entries) {
+        switch (entry.type) {
+            case 'directory': {
+                await fs.mkdir(path.resolve(target, entry.path), {recursive: true});
+                break;
+            }
+            case 'file': {
+                await fs.mkdir(path.resolve(target, path.dirname(entry.path)), {recursive: true});
+                await fs.writeFile(path.resolve(target, entry.path), Buffer.from(entry.content, 'base64'));
+                break;
+            }
+        }
+    }
 };
 
 await restore();
