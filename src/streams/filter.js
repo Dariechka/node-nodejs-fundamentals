17f9d6be98b08ea56e2c09d65b9d9e81c45e54ad
daryakolenchenko <daria.kolenchenko@gmail.com>
2026-03-07 00:04:02 +0100

    feat: implement filter script

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -1,9 +1,38 @@
+import {pipeline, Transform} from "stream";
+import {parseArgs} from "node:util";
+
 const filter = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Filter lines by --pattern CLI argument
-  // Use Transform Stream
-  // Write to process.stdout
+    // Write your code here
+    // Read from process.stdin
+    // Filter lines by --pattern CLI argument
+    // Use Transform Stream
+    // Write to process.stdout
+    const {values} = parseArgs({
+        options: {
+            pattern: {
+                type: 'string',
+            },
+        },
+        allowPositionals: true,
+    });
+    const transformer = new Transform({
+        transform(chunk, encoding, callback) {
+            const result = chunk.toString().split('\n').filter(line => line.includes(values.pattern)).join('\n');
+            if (result) {
+                callback(null, result + '\n');
+            } else {
+                callback();
+            }
+        }
+    })
+
+    pipeline(
+        process.stdin,
+        transformer,
+        process.stdout,
+        Function.prototype,
+    );
+
 };
 
 filter();
