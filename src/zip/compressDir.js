3cbca2e4afb6381240a743d6207a22db70e46001
daryakolenchenko <daria.kolenchenko@gmail.com>
2026-03-08 01:10:48 +0100

    feat: implement compress/decompress script

--- src/zip/compressDir.js
+++ src/zip/compressDir.js
@@ -1,9 +1,88 @@
+import path from "node:path";
+import fs from "fs";
+import fsPromises from "fs/promises";
+import {createBrotliCompress} from "zlib";
+
 const compressDir = async () => {
-  // Write your code here
-  // Read all files from workspace/toCompress/
-  // Compress entire directory structure into archive.br
-  // Save to workspace/compressed/
-  // Use Streams API
+    // Write your code here
+    // Read all files from workspace/toCompress/
+    // Compress entire directory structure into archive.br
+    // Save to workspace/compressed/
+    // Use Streams API
+    const workspace = path.resolve(import.meta.dirname, "workspace");
+    const toCompress = path.resolve(workspace, 'toCompress');
+    const isDir = async p => {
+        try {
+            return (await fsPromises.stat(p)).isDirectory();
+        } catch (error) {
+            return false;
+        }
+    };
+    if (!(await isDir(toCompress))) {
+        throw new Error("FS operation failed");
+    }
+
+    const compressed = path.resolve(workspace, 'compressed');
+    const archive = path.resolve(compressed, 'archive.br');
+
+    await fsPromises.mkdir(compressed, {recursive: true});
+
+
+    const scan = async (dir, entries = []) => {
+        const files = await fsPromises.readdir(dir, {withFileTypes: true});
+
+        for (const file of files) {
+            const filePath = path.resolve(file.parentPath, file.name);
+
+            if (file.isDirectory()) {
+                entries.push({
+                    path: path.relative(toCompress, filePath),
+                    type: 'directory',
+                })
+
+                await scan(filePath, entries);
+            }
+            if (file.isFile()) {
+                const stats = await fsPromises.stat(filePath)
+
+                entries.push({
+                    path: path.relative(toCompress, filePath),
+                    type: 'file',
+                    size: stats.size,
+                })
+            }
+        }
+        return entries;
+    }
+
+    const entries = await scan(toCompress);
+    const json = JSON.stringify(entries);
+
+
+    const out = fs.createWriteStream(archive);
+    const compressor = createBrotliCompress();
+    compressor.pipe(out);
+
+    const buffer = Buffer.alloc(8);
+    buffer.writeBigInt64LE(BigInt(json.length), 0);
+    out.write(buffer);
+    compressor.write(json);
+
+    for (const entry of entries) {
+        if (entry.type === "directory") {
+            continue;
+        }
+
+        const reader = fs.createReadStream(path.resolve(toCompress, entry.path));
+        reader.pipe(compressor, {end: false});
+        await new Promise((resolve, reject) => {
+            reader.on('end', () => resolve());
+            reader.on('error', err => reject(err));
+        });
+    }
+
+    compressor.end();
+    await new Promise((resolve) => out.on('finish', () => resolve()))
 };
 
 await compressDir();
--- src/zip/decompressDir.js
+++ src/zip/decompressDir.js
@@ -1,8 +1,97 @@
+import path from "node:path";
+import fsPromises from "fs/promises";
+import fs from "fs";
+import {createBrotliDecompress} from "zlib";
+
 const decompressDir = async () => {
-  // Write your code here
-  // Read archive.br from workspace/compressed/
-  // Decompress and extract to workspace/decompressed/
-  // Use Streams API
+    // Write your code here
+    // Read archive.br from workspace/compressed/
+    // Decompress and extract to workspace/decompressed/
+    // Use Streams API
+    const workspace = path.resolve(import.meta.dirname, 'workspace');
+    const decompressed = path.resolve(workspace, 'decompressed');
+    const compressed = path.resolve(workspace, 'compressed');
+    const archive = path.resolve(compressed, 'archive.br');
+    try {
+        if (!(await fsPromises.stat(archive)).isFile()) {
+            throw new Error();
+        }
+    } catch {
+        throw new Error('FS operation failed');
+    }
+
+    async function readBytes(stream, n) {
+        const buffers = [];
+        let length = 0;
+
+        return new Promise((resolve, reject) => {
+            const onData = (chunk) => {
+                buffers.push(chunk);
+                length += chunk.length;
+
+                if (length >= n) {
+                    stream.pause();
+
+                    const result = Buffer.concat(buffers, length).slice(0, n);
+                    const leftover = Buffer.concat(buffers, length).slice(n);
+
+                    stream.unshift(leftover);
+                    cleanup();
+                    resolve(result);
+                }
+            }
+
+            const onEnd = () => {
+                const result = Buffer.concat(buffers, length);
+                cleanup();
+                resolve(result);
+            }
+
+            const onError = (err) => {
+                cleanup();
+                reject(err);
+            }
+
+            const cleanup = () => {
+                stream.removeListener('data', onData);
+                stream.removeListener('end', onEnd);
+                stream.removeListener('error', onError);
+            }
+
+            stream.on('data', onData);
+            stream.on('end', onEnd);
+            stream.on('error', onError);
+
+            stream.resume();
+        });
+    }
+
+    const fd = fs.openSync(archive, 'r');
+    const header = Buffer.alloc(8);
+    fs.readSync(fd, header, 0, 8, 0);
+    const jsonLength = Number(header.readBigInt64LE(0));
+    fs.closeSync(fd);
+
+    const reader = fs.createReadStream(archive, {start: 8}).pipe(createBrotliDecompress());
+
+    const jsonBytes = await readBytes(reader, jsonLength);
+    const entries = JSON.parse(jsonBytes);
+
+    for (const entry of entries) {
+        const absolute = path.resolve(decompressed, entry.path);
+        switch (entry.type) {
+            case 'directory': {
+                await fsPromises.mkdir(absolute, {recursive: true});
+                break;
+            }
+            case 'file': {
+                await fsPromises.mkdir(path.dirname(absolute), {recursive: true});
+                const data = await readBytes(reader, entry.size); // todo split to small chunks
+                fs.createWriteStream(absolute).write(data);
+                break;
+            }
+        }
+    }
 };
 
 await decompressDir();
